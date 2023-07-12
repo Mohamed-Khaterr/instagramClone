eaab2cd72c1f9e83083105c2434d01e41a0e0cff
Mohamed Khater <mohamed.15751@gmail.com>
2023-07-12 00:17:07 -0700

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Khater on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
