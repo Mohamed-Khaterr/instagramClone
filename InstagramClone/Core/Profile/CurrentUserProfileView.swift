36e237f35fad7c1fc1553ee11b9398eb4f764517
Mohamed Khater <mohamed.15751@gmail.com>
2023-07-22 07:32:28 -0700

    Create With NavigationStack For Current User

---
+++ InstagramClone/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,45 @@
+//
+//  CurrentUserProfileView.swift
+//  InstagramClone
+//
+//  Created by Khater on 7/22/23.
+//
+
+import SwiftUI
+
+
+struct CurrentUserProfileView: View {
+    
+    private let currentUser: User = User.MOCK_USERS[0]
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                // Header
+                ProfileHeaderView(isCurrentUser: true, user: currentUser)
+                
+                // Post Grid View
+                ProfilePostGridView(posts: Post.MOCK_POSTS)
+                
+                Spacer()
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        print("Sheet Button Pressed")
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
+            }
+        }
+    }
+}
